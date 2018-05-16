2a8d3aa62f5ae8de1abb7252981703d33061ae30
Peter Lanoie <peter.lanoie@cengage.com>
2018-05-16 17:49:23 -0400

    Create empty project.

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Pelasoft.JumpDir
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello world");
+		}
+	}
+}
