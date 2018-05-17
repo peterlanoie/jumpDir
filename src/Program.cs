7248aa8b80f5cbf8abb61a37f390cf7fd8caf3bc
Lanoie, Peter <peter.lanoie@cengage.com>
2018-05-17 18:29:20 -0400

    Reorganize for logging and method breakup. Add back reference check. Change search match to favor starts with before contains.

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Pelasoft.JumpDir
 {
@@ -12,13 +13,22 @@ namespace Pelasoft.JumpDir
 		private const string _userDataDir = ".jumpDir";
 		private const string _userDataFile = "userdata.json";
 
+		private Regex _backRefExp = new Regex(@"(?<backref>(\.\.\\?)+)(?<dir>[^ ]+)?");
+
+		private bool _verbose = true;
+		private string _userDataFilePath;
+		private UserData _userData;
+
+
 		static void Main(string[] args)
 		{
-			Console.Error.WriteLine("Args:");
-			args.ToList().ForEach(x => Console.Error.WriteLine($"   {x}"));
-			Console.Error.WriteLine();
-			var dirs = Directory.GetDirectories(Path.GetFullPath(".")).ToList();
-			Console.Error.WriteLine("Dirs:");
+			new Program().Run(args);
+		}
+
+		private void Run(string[] args)
+		{
+			Verbose(() => $"raw args: { string.Join(' ', args)}\n");
+			//			args.ToList().ForEach(x => Verbose($"   {x}"));
 
 			string homePath = (Environment.OSVersion.Platform == PlatformID.Unix ||
 				   Environment.OSVersion.Platform == PlatformID.MacOSX)
@@ -26,15 +36,13 @@ namespace Pelasoft.JumpDir
 					: Environment.ExpandEnvironmentVariables("%USERPROFILE%");
 
 			var userDataDirPath = Path.Combine(homePath, _userDataDir);
-			var userDataFilePath = Path.Combine(userDataDirPath, _userDataFile);
+			_userDataFilePath = Path.Combine(userDataDirPath, _userDataFile);
 
-			Console.Error.WriteLine($"User data file: {userDataFilePath}");
+			Verbose(() => $"user data file: {_userDataFilePath}\n");
 
-			UserData userData;
-
-			if (File.Exists(userDataFilePath))
+			if (File.Exists(_userDataFilePath))
 			{
-				userData = JsonConvert.DeserializeObject<UserData>(File.ReadAllText(userDataFilePath));
+				_userData = JsonConvert.DeserializeObject<UserData>(File.ReadAllText(_userDataFilePath));
 			}
 			else
 			{
@@ -42,39 +50,112 @@ namespace Pelasoft.JumpDir
 				{
 					Directory.CreateDirectory(userDataDirPath);
 				}
-				userData = new UserData();
+				_userData = new UserData();
 			}
 
-			Func<string, bool> predicate = x => true; // default predicate, take everything
+			var commands = args.Where(x => x.StartsWith('-')).Select(x => x.ToLower().TrimStart('-'));
+			Verbose(() => $"captured command(s): { string.Join(' ', commands) }");
 
-			if (args.Length > 0)
+			args = args.Where(x => !x.StartsWith('-')).ToArray();
+
+			Console.WriteLine(FindDirectory(args.Length > 0 ? args[0] : null));
+		}
+
+		private string FindDirectory(string searchDir)
+		{
+			var targetPath = Path.GetFullPath(".");
+
+			if (searchDir != null)
 			{
-				dirs = dirs.Where(x => x.ToLower().Contains(args[0].ToLower())).ToList();
+				Verbose(() => $"finding possible directories for: {searchDir}");
+				var backRefMatch = _backRefExp.Match(searchDir);
+
+				if (backRefMatch.Success)
+				{
+					var backRef = backRefMatch.Groups["backref"].Value;
+					Verbose(() => $"found backreference: {backRef}");
+					targetPath = Path.GetFullPath(backRef);
+					Verbose(() => $"updated target search path to: {targetPath}\n");
+					var dirGroup = backRefMatch.Groups["dir"];
+					searchDir = dirGroup?.Value;
+				}
 			}
-			if (dirs.Count() == 1)
+			var dirs = Directory.GetDirectories(targetPath)
+				.Select(x => x.Replace(targetPath, "", StringComparison.InvariantCultureIgnoreCase));
+			if (searchDir != null)
 			{
-				var targetPath = dirs.First();
-				Console.WriteLine(targetPath);
-				var entry = userData.Entries.FirstOrDefault(x => x.Path == targetPath);
-				if (entry == null)
+				var searches = new Func<string, string, bool>[]
 				{
-					userData.Entries.Add(entry = new Entry { Path = targetPath, Rank = 1 });
-				}
-				else
+					(dir, search) => dir.StartsWith(search, StringComparison.InvariantCultureIgnoreCase), // 1. dirs that START with the arg
+					(dir, search) => dir.ToLower().Contains(search.ToLower()),   // 2. dirs that CONTAIN the arg
+				};
+
+				foreach (var search in searches)
 				{
-					entry.Rank++;
+					var results = dirs.Where(dir => search(dir, searchDir)).ToArray();
+					if (results.Count() == 1)
+					{
+						// if theres 1, go to it
+						return ChangeDirectory(results.First());
+					}
+					else if (results.Count() > 1)
+					{
+						// if there's more than 1, update the main list
+						dirs = results;
+					}
 				}
-				userData.LastPath = targetPath;
-				File.WriteAllText(userDataFilePath, JsonConvert.SerializeObject(userData, Formatting.Indented));
-				return;
 			}
+
 			if (dirs.Count() > 1)
 			{
-				Console.Error.WriteLine("Possible targets:");
-				dirs.ForEach(x => Console.Error.WriteLine($"   {x}"));
+				Log("Possible targets:");
+				dirs.ToList().ForEach(x => Log($"   {x}"));
+			} else
+			{
+				Log("Sorry, no target directories found");
 			}
-			Console.WriteLine("[no target]");
+			return "[no target]";
+		}
+
+		private string ChangeDirectory(string path)
+		{
+			var entry = _userData.Entries.FirstOrDefault(x => x.Path == path);
+			if (entry == null)
+			{
+				_userData.Entries.Add(entry = new Entry { Path = path, Rank = 1 });
+			}
+			else
+			{
+				entry.Rank++;
+			}
+			_userData.LastPath = path;
+			File.WriteAllText(_userDataFilePath, JsonConvert.SerializeObject(_userData, Formatting.Indented));
+			return path;
+		}
+
+		private void SaveData()
+		{
 
 		}
+
+		//private void Verbose(string message = null)
+		//{
+		//	Verbose(() => message);
+		//}
+
+		private void Verbose(Func<string> messageFunc)
+		{
+			if (_verbose)
+			{
+				Console.Error.WriteLine(messageFunc());
+			}
+		}
+
+
+		private void Log(string message = null)
+		{
+			Console.Error.WriteLine(message);
+		}
+
 	}
 }
