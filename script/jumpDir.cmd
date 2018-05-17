17b6a5feabf5e8ae79c6b125e8434fcb3c0bb44c
Lanoie, Peter <peter.lanoie@cengage.com>
2018-05-17 18:30:02 -0400

    Build to just the bin to simplify.

--- script/jumpDir.cmd
+++ script/jumpDir.cmd
@@ -1,3 +1,3 @@
 @echo off
-for /f "tokens=*" %%i in ('dotnet %~dp0..\src\bin\Debug\netcoreapp2.0\Pelasoft.JumpDir.dll %*') do set JUMPDIRRESULT=%%i
+for /f "tokens=*" %%i in ('dotnet %~dp0..\src\bin\netcoreapp2.0\Pelasoft.JumpDir.dll %*') do set JUMPDIRRESULT=%%i
 if not "%JUMPDIRRESULT%" == "[no target]" cd %JUMPDIRRESULT%
