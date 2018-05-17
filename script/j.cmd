153ab4f151f2d03ba178bff7f5bd466081a9e4da
Peter Lanoie <peter.lanoie@cengage.com>
2018-05-17 00:40:19 -0400

    Clean up main script to reference binary relatively.

--- script/j.cmd
+++ script/j.cmd
@@ -1,5 +1,3 @@
 @echo off
-for /f "tokens=*" %%i in ('dotnet D:\Pelasoft\Pelasoft.JumpDir\bin\Debug\netcoreapp2.0\Pelasoft.JumpDir.dll %*') do set JUMPDIRRESULT=%%i
-::echo %JUMPDIRRESULT%
+for /f "tokens=*" %%i in ('dotnet %~dp0..\src\bin\Debug\netcoreapp2.0\Pelasoft.JumpDir.dll %*') do set JUMPDIRRESULT=%%i
 if not "%JUMPDIRRESULT%" == "[no target]" cd %JUMPDIRRESULT%
-::dotnet D:\Pelasoft\Pelasoft.JumpDir\bin\Debug\netcoreapp2.0\Pelasoft.JumpDir.dll %*
