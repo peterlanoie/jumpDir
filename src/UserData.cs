54b4e795ca32a16443c0b194959f7f3a951693c1
Lanoie, Peter <peter.lanoie@cengage.com>
2018-05-18 00:29:57 -0400

    Add props to capture last usage data for reuse on subsequent calls.

--- src/UserData.cs
+++ src/UserData.cs
@@ -10,6 +10,8 @@ namespace Pelasoft.JumpDir
 
 		public string LastPath { get; set; }
 		public string LastSearch { get; set; }
+		public List<string> LastCandidates { get; set; }
+		public DateTime LastAccess { get; set; }
 
 		public UserData()
 		{
