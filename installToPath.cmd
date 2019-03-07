754cae9cb5482cb9dd31863d0701ed7ba338da4a
Peter Lanoie <planoie@mvphealthcare.com>
2019-03-07 10:14:01 -0500

    System path modification probably is overkill. Remove it. Fix message typo.

--- installToPath.cmd
+++ installToPath.cmd
@@ -2,7 +2,7 @@
 where jd.cmd /Q
 ::echo where check returned %ERRORLEVEL%
 IF %ERRORLEVEL%==0 (
-	echo `jd.cmd` appears to already be in the path. Nothing to else add.
+	echo `jd.cmd` appears to already be in the path. Nothing else to add.
 	goto end
 )
 echo Can't find `jd` command in current path.
@@ -10,13 +10,14 @@ echo.
 echo Command search path to add: %~dp0script
 echo.
 echo Possible path variable locations:
-echo   M - Machine/system wide path (persistent) and current session
+::echo   M - Machine/system wide path (persistent) and current session
+::echo       This requires execution as admin. It will fail otherwise.
 echo   U - User path (persistent) and current session
-echo   C - Current command session only (temporary)
+echo   S - Current command session only (temporary)
 echo.
-choice /C cumq /N /M "Which path to add to (or Q to skip)? "
-if ERRORLEVEL 4 goto end
-if ERRORLEVEL 3 goto setmachinepath
+choice /C suq /N /M "Which path to add to (or Q to skip)? "
+if ERRORLEVEL 3 goto end
+::if ERRORLEVEL 3 goto setmachinepath
 if ERRORLEVEL 2 goto setuserpath
 if ERRORLEVEL 1 goto setlocalpath
 goto end
@@ -27,11 +28,18 @@ SETX PATH "%~dp0script;%PATH%"
 goto setlocalpath
 
 :setmachinepath
+::echo !!! Not implemented yet.
+SET tmp_RegKey="HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+FOR /F "usebackq tokens=2*" %%A IN (`REG QUERY %tmp_RegKey% /v PATH`) DO Set tmp_CurrPath=%%B
+ECHO %tmp_CurrPath% > system_path_bak.txt
+ECHO Current system path saved to `system_path_bak.txt`.
 echo Setting to system wide environment variable.
-::SETX /M PATH "%~dp0script;%PATH%"
+SETX PATH "%CurrPath%";%1 /M
+SET tmp_RegKey=
+SET tmp_CurrPath=
 
 :setlocalpath
-echo Setting to current command session only.
+echo Setting to current command session.
 SET "PATH=%~dp0script;%PATH%"
 goto end
 
